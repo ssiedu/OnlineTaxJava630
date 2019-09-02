f2fb3d9302005b8f05c74b889e9abb2a415ea216
manoj <manoj.sarwate@ssiedu.in>
2019-09-02 18:42:49 +0530

    02-09.19

--- src/java/TaxCalculator.java
+++ src/java/TaxCalculator.java
@@ -22,11 +22,34 @@ public class TaxCalculator extends HttpServlet {
             tax=income*10/100;
         }
         //provides the response
-        PrintWriter pw=response.getWriter();
-        pw.println("Thanks For Visiting Us.");
-        pw.println("Your Tax Liability Is : "+tax);
-        pw.println("Pay Your Taxes Before 31st March");
-        pw.close();
+        PrintWriter out=response.getWriter();
+        
+        out.println("<html>");
+        out.println("<body bgcolor=pink>");
+        out.println("<hr>");
+        out.println("<h3>Income Tax Dept</h3>");
+        out.println("<h4>Thanks For Visiting Us</h4>");
+        out.println("<hr>");
+        out.println("<table border=2>");
+        out.println("<tr>");
+        out.println("<td>Income</td>");
+        out.println("<td>"+income+"</td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<td>Age</td>");
+        out.println("<td>"+age+"</td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<td>Tax</td>");
+        out.println("<td>"+tax+"</td>");
+        out.println("</tr>");
+        out.println("</table>");
+        out.println("<hr>");
+        out.println("<marquee>Be A Responsible Citizen. Pay Taxes On Time Honestly</marquee>");
+        out.println("</body>");
+        out.println("</html>");
+        
+        out.close();
         
     }
 
